05c7109031cbe0960da75b798521d777214535b9
Cass <casschin@gmail.com>
2017-07-09 13:04:20 -0400

    link style fix

--- src/javascripts/components/footer.js
+++ src/javascripts/components/footer.js
@@ -1,17 +1,28 @@
-import '../../stylesheets/components/footer.css';
+import "../../stylesheets/components/footer.css";
 
-import React from 'react';
-import tmdbLogo from '../../images/tmdb.svg';
+import React from "react";
+import tmdbLogo from "../../images/tmdb.svg";
 
 const Footer = () => {
   return (
     <footer className="Footer">
       <div className="Footer-tmdbLogo">
-        <a href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.themoviedb.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={tmdbLogo} alt="The Movie Database Logo" />
         </a>
       </div>
-      <span className="Footer-itMe">Cass Chindusties 2017</span>
+      <a
+        className="Footer-itMe"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/casschin/same-movie"
+      >
+        Cass Chindusties 2017
+      </a>
     </footer>
   );
 };
